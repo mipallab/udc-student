5f11fd09c8158ddf67a547037a8836bf14bc6131
pallab <pallab4842@gmail.com>
2023-05-15 23:42:40 +0600

    signup update 15/5/23

--- pages/signup.php
+++ pages/signup.php
@@ -5,6 +5,7 @@ include_once('../autoload.php');
 
 	if(isset($_POST['submit'])) {
 
+		$error = array();
 		//POST data
 			$fullName 				= $_POST['full_name'] ;
 		    $fatherName 			= $_POST['fathers_name'];
@@ -19,24 +20,56 @@ include_once('../autoload.php');
 		    $username 				= $_POST['username'] ;
 		    $new_password			= $_POST['new-password'] ;
 		    $confarm_password		= $_POST['confarm-password'] ;
+
+		//confarm password match
+		    if($new_password != $confarm_password) {
+		    	$error['con_password'] = 'Password not match!';
+		    }
 		    
+	    //interested subject
+		    $interested = $_POST['interested_sub'];
+			$chk = '';
+			foreach($interested as $chk1)  
+		   	{  
+		      $chk .= $chk1.", ";  
+		   	}  
+		   	$interested_sub = substr($chk,0,-2);
+
+		//image setup
+		   	$photo_name = $_FILES['up_photo']['name'];
+		   	$photo_type = $_FILES['up_photo']['type'];
+		   	$photo_size = $_FILES['up_photo']['size'];
+		   	$photo_tmp = $_FILES['up_photo']['tmp_name'];
+		   	$photo_error = $_FILES['up_photo']['error'];
+
+		   	
+
+
+		   	if($photo_size > 5242880) {
+		   		$error['large_photo'] = 'Your file size too long';
+		   	}
+
+
 		     echo "<pre>";
 		     print_r($_POST);
+		     echo "</pre> <br> <br> <br> <br> ";
+
+		     echo "<pre>";
+		     print_r($_FILES);
 		     echo "</pre>";
 
 
-		$interested = $_POST['interested_sub'];
-		$chk = '';
-		foreach($interested as $chk1)  
-	   	{  
-	      $chk .= $chk1.", ";  
-	   	}  
+		if(count($error) === 0) {
 
-	   	$interested_sub = substr($chk,0,-2);
+			//send data
+				move_uploaded_file($photo_tmp, "../assects/media/img/users/".$photo_name);
 
-		//send query
-			$sql = "INSERT INTO students(full_name, father_name, mother_name, present_address, date_of_birth, age, occupation, phone, interested_subject, gender, email, username, password) VALUES ('$fullName', '$fatherName', '$motherName', '$address', '$dob', '$old', '$occopation', '$phone', '$interested_sub', '$gender', '$email', '$username', '$new_password')";
-			mysqli_query($connect, $sql) or die('query not send');
+			//send query
+				$sql = "INSERT INTO students(full_name, father_name, mother_name, present_address, date_of_birth, age, occupation, phone, interested_subject, gender, email, username, password, photo) VALUES ('$fullName', '$fatherName', '$motherName', '$address', '$dob', '$old', '$occopation', '$phone', '$interested_sub', '$gender', '$email', '$username', '$new_password', '$photo_name')";
+				mysqli_query($connect, $sql) or die('query not send');
+
+		}
+
 	}
 
 ?>
@@ -73,54 +106,54 @@ include_once('../autoload.php');
 					<h3>Student Sign Up From</h3>
 				</div>
 				<div class="card-body">
-					<form action="#" method="POST" enctype="multi-part/form-data">
+					<form action="#" method="POST" enctype="multipart/form-data">
 						<!-- full name field -->
 						<div class="fullNameField mb-3">
 							<label class="form-label" for="fullname mt-2">Full Name</label> <br>
-							<input class="px-2 form-control" id="fullname" type="text" required name="full_name">
+							<input class="px-2 form-control" id="fullname" type="text" required name="full_name" value="<?php echo (isset($fullName)) ? ($fullName) : ("");?>">
 							<span class="form-text">Must match NID or DOB card</span>
 						</div>
 
 						<!-- father name field -->
 						<div class="fatherNameField mb-3">
 							<label class="form-label" for="fathersname mt-2">Father's Name</label><br>
-							<input class="px-2 form-control" id="fathersname" type="text" required name="fathers_name">
+							<input class="px-2 form-control" id="fathersname" type="text" required name="fathers_name" value="<?php echo (isset($fatherName)) ? ($fatherName) : ("");?>">
 						</div>
 
 						<!-- mother name filed -->
 						<div class="motherNameField mb-3">
 							<label class="form-label" for="mothersname mt-2">Mother's Name</label><br>
-							<input class="px-2 form-control" id="mothersname" type="text" required name="mothers_name">
+							<input class="px-2 form-control" id="mothersname" type="text" required name="mothers_name" value="<?php echo (isset($motherName)) ? ($motherName) : ("");?>">
 						</div>
 
 						<!-- present address filed -->
 						<div class="presentAddress mb-3">
 							<label class="form-label" for="address mt-2">Present Address</label><br>
-							<input class="px-2 form-control" id="address" type="address" required name="address">
+							<input class="px-2 form-control" id="address" type="address" required name="address" value="<?php echo (isset($address)) ? ($address) : ("");?>">
 						</div>
 						
 						<!-- Date of birth filed -->
 						<div class="nameField mb-3">
 							<label class="form-label" for="dofdate mt-2">Date of Birth</label><br>
-							<input class="px-2 form-control" id="dofdate" onchange="ageCalculator()" type="date" required name="date_of_birth">
+							<input class="px-2 form-control" id="dofdate" onchange="ageCalculator()" type="date" required name="date_of_birth" value="<?php echo (isset($dob)) ? ($dob) : ("");?>">
 						</div>
 
 						<!-- old field -->
 						<div class="oldField mb-3">
 							<label class="form-label" for="old mt-2">Your Old</label><br>
-							<input class="px-2 form-control old"  id="old" type="text" required readonly name="old">
+							<input class="px-2 form-control old"  id="old" type="text" required readonly name="old" value="<?php echo (isset($old)) ? ($old) : ("");?>">
 						</div>
 
 						<!-- occopation field -->
 						<div class="nameField mb-3">
 							<label class="form-label" for="occopation mt-2">Occopation</label><br>
-							<input class="px-2 form-control" id="occopation" type="text" required name="occopation">
+							<input class="px-2 form-control" id="occopation" type="text" required name="occopation" value="<?php echo (isset($occopation)) ? ($occopation) : ("");?>">
 						</div>
 
 						<!-- phone filed -->
 						<div class="nameField mb-3">
 							<label class="form-label" for="nubmer mt-2">Phone Number</label><br>
-							<input class="px-2 form-control" id="nubmer" type="tel" required name="phone">
+							<input class="px-2 form-control" id="nubmer" type="tel" required name="phone" value="<?php echo (isset($phone)) ? ($phone) : ("");?>">
 						</div>
 
 						<!-- interested subject -->
@@ -160,11 +193,11 @@ include_once('../autoload.php');
 							<fieldset class="border p-2">
 								<legend  class="float-none w-auto p-2 fs-6">Gender</legend>
 								<div class="form-check form-check-inline">
-									<input class="form-check-input" type="radio" name="gender" id="male" value="male">
+									<input class="form-check-input" type="radio" name="gender" id="male" value="male" alue="<?php echo (isset($gender)) ? ($phone) : ("");?>">
 									<label class="form-check-label" for="male">Male</label>
 								</div>
 								<div class="form-check form-check-inline">
-									<input class="form-check-input" type="radio" name="gender" id="female" value="female">
+									<input class="form-check-input" type="radio" name="gender" id="female" value="female" alue="<?php echo (isset($gender)) ? ($phone) : ("");?>">
 									<label class="form-check-label" for="female">Female</label>
 								</div>
 							 </fieldset>
@@ -173,26 +206,32 @@ include_once('../autoload.php');
 						<!-- email filed -->
 						<div class="emailField mb-3">
 							<label class="form-label" for="email mt-2">Email</label><br>
-							<input class="px-2 form-control" id="email" type="email" name="email" required>
+							<input class="px-2 form-control" id="email" type="email" name="email" required value="<?php echo (isset($email)) ? ($email) : ("");?>">
 						</div>
 
 						<!-- username -->
 						<div class="userField mb-3">
 							<label class="form-label" for="username mt-2">Username</label><br>
-							<input class="px-2 form-control" id="username" type="text" name="username" required>
+							<input class="px-2 form-control" id="username" type="text" name="username" required value="<?php echo (isset($username)) ? ($username) : ("");?>">
 						</div>
 
 						<!-- new password -->
 						<div class="userPass mb-3">
 							<label class="form-label" for="password">Password</label><br>
-							<input class="px-2 form-control" id="password" type="password" name="new-password" required>
+							<input class="px-2 form-control" id="password" type="password" name="new-password" required value="<?php echo (isset($new_password)) ? ($new_password) : ("");?>">
 						</div>
 
 						<!-- comfarm password -->
 						<div class="userConPass mb-3">
 							<label class="form-label" for="conpass">Comfarm Password</label><br>
-							<input class="px-2 form-control" id="conpass" type="password" name="confarm-password" required>
-							<div class="form-text">We'll never share your password with anyone else.</div>
+							<input class="px-2 form-control" id="conpass" type="password" name="confarm-password" required value="<?php echo (isset($confarm_password)) ? ($confarm_password) : ("");?>">
+							<div class="form-text text-danger">
+								<?php
+									if(isset($error['con_password'])) {
+										echo $error['con_password'];
+									}
+								?>
+							</div>
 						</div>
 
 						<!-- photo upload -->
@@ -202,7 +241,7 @@ include_once('../autoload.php');
 							    	<label for="imgfile" class="col-form-label">Upload your Photo</label>
 							  	</div>
 							  	<div class="col-auto">
-							    	<input type="file" id="imgfile" class=" image fileimg form-control" aria-describedby="fileHelpInline"name="photo" required>
+							    	<input type="file" id="imgfile" class=" image fileimg form-control" aria-describedby="fileHelpInline" name="up_photo" required>
 							  	</div>
 							  	<div class="col-auto">
 							    	<span id="fileHelpInline" class="form-text">
@@ -212,6 +251,13 @@ include_once('../autoload.php');
 							</div>
 							<div class="photo border m-3 p-3">
 								<img class="livephoto  border" src="../assects/media/img/dammy.png" alt="Dammy Pic" >
+								<div class="form-text text-danger">
+								<?php
+									if(isset($error['large_photo'])) {
+										echo $error['large_photo'];
+									}
+								?>
+							</div>
 							</div>
 						</div>
 						<input class="mt-3 btn btn-primary" type="submit" value="Sign up" name="submit">
